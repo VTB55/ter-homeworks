68cd7209f038e78732494354f8ef154cfecfc91b
Sergey Konovalov <s9039273703l@gmail.com>
2025-10-24 00:12:18 +0600

    Added MySQL Docker container task with random passwords

---
+++ mysql-docker-task/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Генерируем случайные пароли
+resource "random_password" "mysql_root_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "random_password" "mysql_user_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+# Создаем docker образ mysql
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
+  keep_locally = true
+}
+
+# Запускаем контейнер mysql
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name  = "mysql-${random_password.mysql_root_password.result}"
+  
+  ports {
+    internal = 3306
+    external = 3306
+    ip       = "127.0.0.1"
+  }
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.mysql_root_password.result}",
+    "MYSQL_DATABASE=wordpress", 
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.mysql_user_password.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+  restart = "unless-stopped"
+}
